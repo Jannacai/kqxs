70fd0f16771fa4f6da0824ee9ff9e5e4949d68e6
Vuong Name <quangvuongnqv@example.com>
2025-05-31 23:40:01 +0700

    lan16

--- next.config.js
+++ next.config.js
@@ -9,12 +9,12 @@ module.exports = {
     },
     async redirects() {
         return [
-            { source: '/xsmb', destination: '/ket-qua-xo-so-mien-bac' },
-            { source: '/xosomn', destination: '/ket-qua-xo-so-mien-nam' },
-            { source: '/xosomt', destination: '/ket-qua-xo-so-mien-trung' },
-            { source: '/soicauMB', destination: '/soicau/soi-cau-mien-bac' },
-            { source: '/soicauMT', destination: '/soicau/soi-cau-mien-trung' },
-            { source: '/TaoDan', destination: '/tao-dan-dac-biet' },
+            { source: '/xsmb', destination: '/ket-qua-xo-so-mien-bac', permanent: true },
+            { source: '/xosomn', destination: '/ket-qua-xo-so-mien-nam', permanent: true },
+            { source: '/xosomt', destination: '/ket-qua-xo-so-mien-trung', permanent: true },
+            { source: '/soicauMB', destination: '/soicau/soi-cau-mien-bac', permanent: true },
+            { source: '/soicauMT', destination: '/soicau/soi-cau-mien-trung', permanent: true },
+            { source: '/TaoDan', destination: '/tao-dan-dac-biet', permanent: true },
         ];
     },
 };
