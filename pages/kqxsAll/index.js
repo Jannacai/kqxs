a24947f715247e78e17ba7e3af9bae36af2c8e43
Vuong Name <quangvuongnqv@example.com>
2025-06-06 19:25:54 +0700

    ls

--- pages/kqxsAll/index.js
+++ pages/kqxsAll/index.js
@@ -27,9 +27,9 @@ const KQXS = (props) => {
     const [currentPage, setCurrentPage] = useState(1);
     const [isLiveWindow, setIsLiveWindow] = useState(false);
     const [hasTriggeredScraper, setHasTriggeredScraper] = useState(false);
-    const hour = 18;
-    const minute1 = 16;
-    const minute2 = 26;
+    const hour = 19;
+    const minute1 = 27;
+    const minute2 = 28;
 
     const router = useRouter();
     const dayof = props.data4;
